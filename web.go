8c85f1d24b2c9c7be9060303e4d8a86305de5485
furuya <furuya.to@uhmfg.co.jp>
2022-07-30 15:42:32 +0900

    アドレスに関数を割り付け

--- web.go
+++ web.go
@@ -5,6 +5,15 @@ import (
 )
 
 func main () {
-	// http.FileServer( <<FileSystem>> )
-	http.ListenAndServe("", http.FileServer(http.Dir(".")))	// httpパッケージのDir関数
+	// 実行する処理をまとめた関数をhhに用意
+	hh := func(w http.ResponseWriter, rq *http.Request){
+		// ResponseWriterの「Write」メソッドを使って、レスポンスにテキストを出力している
+		w.Write([]byte("Hello, This is GO-server!!"))	// 引数には出力する値をbyte配列として用意 ← クライアントに送られて表示される。
+	}
+	// 作成した関数は、HandleFuncを使い、/helloアドレスに割り付けれられる。
+	// '/hello' でhh関数を実行する
+	http.Handle("/hello", http.HandlerFunc(hh))
+
+	// サーバー実行
+	http.ListenAndServe("", nil)
 }
