8e4a4efeae506e177682a83f8f4e3895e422b362
furuya <furuya.to@uhmfg.co.jp>
2022-07-30 15:51:29 +0900

    HTMLを表示

--- web.go
+++ web.go
@@ -5,6 +5,20 @@ import (
 )
 
 func main () {
-	// http.FileServer( <<FileSystem>> )
-	http.ListenAndServe("", http.FileServer(http.Dir(".")))	// httpパッケージのDir関数
+	// `` →  改行を含むテキストリテラルを記述するのに使われる。
+	// HTMLソースコードのstringをWriteで書き出せば、それがそのままwebブラウザでHTMLとして認識され、表示される。
+	msg := `
+			<html><body>
+			<h1>Hello</h1>
+			<p>This is GO-server!!</p>
+			</body></html>
+	`
+
+	hh := func(w http.ResponseWriter, rq *http.Request) {
+		w.Write([]byte(msg))
+	}
+
+	http.Handle("/hello", http.HandlerFunc(hh))
+
+	http.ListenAndServe("", nil)
 }
